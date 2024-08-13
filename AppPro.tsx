50e5b4a4c1429354fac936ef82136d1a4be4005a
yuvithakur007 <01yuvisingh@gmail.com>
2024-08-13 17:12:56 +0530

    first commit

---
+++ AppPro.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { View, SafeAreaView, Text, StyleSheet, useColorScheme } from "react-native"
+
+function AppPro(): JSX.Element{
+    const isDarkMode = useColorScheme() === 'dark'
+
+    return (
+        <View style={styles.container}>
+            <Text style={isDarkMode? styles.whiteText : styles.darkText}>Hello World </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    whiteText: {
+        color: '#FFFFFF',
+        fontSize: 24,
+    },
+    darkText: {
+        color: '#000000',
+        fontSize: 24,
+    },
+})
+
+
+export default AppPro
