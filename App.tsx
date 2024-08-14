627bcdf5f018c70185be56ad0df5508f25db48e2
yuvithakur007 <01yuvisingh@gmail.com>
2024-08-14 11:05:59 +0530

    added horizonal cards scroll

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { SafeAreaView, Text, ScrollView } from 'react-native';
 import FlatCards from './components/FlatCards';
+import ElevatedCard from './components/ElevatedCard';
 
 function App() {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>Hello World </Text>
         <FlatCards />
+        <ElevatedCard />
       </ScrollView>
     </SafeAreaView>
   )
