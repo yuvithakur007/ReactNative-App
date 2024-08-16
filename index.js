e60ec90e7a769d1c132860efbee36f09a5522178
yuvithakur007 <01yuvisingh@gmail.com>
2024-08-16 17:35:57 +0530

    added password generator

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import {AppRegistry} from 'react-native';
-import App from './App';
+
+// app 1 - getting started
 // import AppPro from './AppPro';
+
+// app 2 - cards component ans styling 
+// import App from './App';
+
+
+// app 3 - password generator
+import PasswordGenerator from './PasswordGenerator';
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => PasswordGenerator);
